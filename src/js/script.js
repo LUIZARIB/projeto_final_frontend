9e5e64c388242837989a100eb37ba8e9a8bbfb97
David Carvalho <davidsoares585@gmail.com>
2023-01-02 20:37:17 -0300

    header e menu do index finalizados

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('btn-div');
+var container = document.querySelector('.div-menu');
+btn.addEventListener('click', function() {
+    
+  if(container.style.display === 'block') {
+    
+      container.style.display = 'none';
+  } else {
+      container.style.display = 'block';
+  }
+});
